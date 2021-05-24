e31f76b86e04bc06f6ca958df2fbfb059dfdcd06
revenmartin <revenmartin@gmail.com>
2021-05-24 19:01:26 +0800

    Update copyright

--- desktopiconprovider.cpp
+++ desktopiconprovider.cpp
@@ -29,22 +29,18 @@ DesktopIconProvider::DesktopIconProvider()
 QPixmap DesktopIconProvider::requestPixmap(const QString &id, QSize *realSize,
                                            const QSize &requestedSize)
 {
-    // Sanitize requested size
     QSize size(requestedSize);
     if (size.width() < 1)
         size.setWidth(1);
     if (size.height() < 1)
         size.setHeight(1);
 
-    // Return real size
     if (realSize)
         *realSize = size;
 
-    // Is it a path?
     if (id.startsWith(QLatin1Char('/')))
         return QPixmap(id).scaled(size);
 
-    // Return icon from theme or fallback to a generic icon
     QIcon icon = QIcon::fromTheme(id);
     if (icon.isNull())
         icon = QIcon::fromTheme(QLatin1String("application-x-desktop"));
