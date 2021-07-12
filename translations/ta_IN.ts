fd3741752c909788306a93f3a9ec5071f6bf89a4
Hosted Weblate <hosted@weblate.org>
2021-07-12 20:38:06 +0200

    Added translation using Weblate (Tamil)
    Translated using Weblate (Nepali)
    
    Currently translated at 100.0% (64 of 64 strings)
    
    Co-authored-by: Hosted Weblate <hosted@weblate.org>
    Co-authored-by: Weblate <noreply@weblate.org>
    Co-authored-by: samee-np <sameerneupane92@gmail.com>
    Translate-URL: https://hosted.weblate.org/projects/cutefishos/filemanager/ne/
    Translation: CutefishOS/FileManager

--- translations/ta_IN.ts
+++ translations/ta_IN.ts
@@ -1,27 +1,27 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="ne">
+<TS version="2.1" language="ta">
 <context>
     <name>CreateFolderDialog</name>
     <message>
         <location filename="../qml/Dialogs/CreateFolderDialog.qml" line="28"/>
         <source>New folder name</source>
-        <translation>नयाँ फोल्डर नाम</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../qml/Dialogs/CreateFolderDialog.qml" line="55"/>
         <source>New folder</source>
-        <translation>नयाँ फोल्डर</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../qml/Dialogs/CreateFolderDialog.qml" line="70"/>
         <source>Cancel</source>
-        <translation>रद्द</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../qml/Dialogs/CreateFolderDialog.qml" line="76"/>
         <source>OK</source>
-        <translation>हुन्छ</translation>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
@@ -29,37 +29,37 @@
     <message>
         <location filename="../helper/datehelper.cpp" line="15"/>
         <source>Now</source>
-        <translation>अहिले</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../helper/datehelper.cpp" line="17"/>
         <source>1 minute ago</source>
-        <translation>1 मिनेटअघि</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../helper/datehelper.cpp" line="19"/>
         <source>%1 minutes ago</source>
-        <translation>%1 मिनेटअघि</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../helper/datehelper.cpp" line="23"/>
         <source>1 hour ago</source>
-        <translation>1 घन्टा अघि</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../helper/datehelper.cpp" line="25"/>
         <source>%1 hours ago</source>
-        <translation>%1 घन्टा अघि</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../helper/datehelper.cpp" line="29"/>
         <source>1 day ago</source>
-        <translation>1 दिन अघि</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../helper/datehelper.cpp" line="31"/>
         <source>%1 days ago</source>
-        <translation>%1 दिन अघि</translation>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
@@ -67,7 +67,7 @@
     <message>
         <location filename="../desktop/desktopview.cpp" line="44"/>
         <source>Desktop</source>
-        <translation>डेस्कटप</translation>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
@@ -75,22 +75,22 @@
     <message>
         <location filename="../qml/Dialogs/EmptyTrashDialog.qml" line="30"/>
         <source>File Manager</source>
-        <translation>फाइल सम्पादक</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../qml/Dialogs/EmptyTrashDialog.qml" line="60"/>
         <source>Do you want to permanently delete all files from the Trash?</source>
-        <translation>के सबै हटाउन चाहनुहुन्छ ?</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../qml/Dialogs/EmptyTrashDialog.qml" line="69"/>
         <source>Cancel</source>
-        <translation>रद्द</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../qml/Dialogs/EmptyTrashDialog.qml" line="75"/>
         <source>Empty Trash</source>
-        <translation>रद्दी खाली गर्ने</translation>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
@@ -98,82 +98,82 @@
     <message>
         <location filename="../model/foldermodel.cpp" line="166"/>
         <source>%1 item</source>
-        <translation>%1 वस्तु</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../model/foldermodel.cpp" line="166"/>
         <source>%1 items</source>
-        <translation>%1 वस्तुहरु</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../model/foldermodel.cpp" line="830"/>
         <source>Select All</source>
-        <translation>सबै चयन</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../model/foldermodel.cpp" line="1042"/>
         <source>Open</source>
-        <translation>खोल्ने</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../model/foldermodel.cpp" line="1045"/>
         <source>Cut</source>
-        <translation>रित्याएर प्रतिलिपी</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../model/foldermodel.cpp" line="1048"/>
         <source>Copy</source>
-        <translation>प्रतिलिपि</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../model/foldermodel.cpp" line="1051"/>
         <source>Paste</source>
-        <translation>टाँस्ने</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../model/foldermodel.cpp" line="1054"/>
         <source>New Folder</source>
-        <translation>नयाँ फोल्डर</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../model/foldermodel.cpp" line="1057"/>
         <source>Move To Trash</source>
-        <translation>रद्दी टोकरी मा सार्ने</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../model/foldermodel.cpp" line="1060"/>
         <source>Empty Trash</source>
-        <translation>रद्दी टोकरी खाली गर्ने</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../model/foldermodel.cpp" line="1063"/>
         <source>Delete</source>
-        <translation>हटाउने</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../model/foldermodel.cpp" line="1066"/>
         <source>Rename</source>
-        <translation>पुन:नामाकरण</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../model/foldermodel.cpp" line="1069"/>
         <source>Open in Terminal</source>
-        <translation>टर्मिनल मा खोल्ने</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../model/foldermodel.cpp" line="1072"/>
         <source>Set as Wallpaper</source>
-        <translation>वलपेपर लगाउने</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../model/foldermodel.cpp" line="1075"/>
         <source>Properties</source>
-        <translation>स्वामित्व</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../model/foldermodel.cpp" line="1078"/>
         <source>Change background</source>
-        <translation>पृष्ठभुमि बदल्ने</translation>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
@@ -181,37 +181,37 @@
     <message>
         <location filename="../qml/FolderPage.qml" line="54"/>
         <source>Empty folder</source>
-        <translation>फोल्डर खाली छ</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../qml/FolderPage.qml" line="85"/>
         <source>Open</source>
-        <translation>खोल्ने</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../qml/FolderPage.qml" line="90"/>
         <source>Properties</source>
-        <translation>स्वामित्व</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../qml/FolderPage.qml" line="146"/>
         <source>%1 item</source>
-        <translation>%1 वस्तु</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../qml/FolderPage.qml" line="147"/>
         <source>%1 items</source>
-        <translation>%1 वस्तुहरु</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../qml/FolderPage.qml" line="153"/>
         <source>%1 selected</source>
-        <translation>%1 चयन गरियो</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../qml/FolderPage.qml" line="164"/>
         <source>Empty Trash</source>
-        <translation>रद्दी टोकरी खाली गर्ने</translation>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
@@ -219,27 +219,27 @@
     <message>
         <location filename="../qml/OptionsMenu.qml" line="45"/>
         <source>Icons</source>
-        <translation>आइकनहरु</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../qml/OptionsMenu.qml" line="79"/>
         <source>List</source>
-        <translation>सुची</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../qml/OptionsMenu.qml" line="115"/>
         <source>Name</source>
-        <translation>नाम</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../qml/OptionsMenu.qml" line="149"/>
         <source>Date</source>
-        <translation>मिति</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../qml/OptionsMenu.qml" line="183"/>
         <source>Size</source>
-        <translation>आकार</translation>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
@@ -247,42 +247,42 @@
     <message>
         <location filename="../model/placesmodel.cpp" line="39"/>
         <source>Home</source>
-        <translation>गृह</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../model/placesmodel.cpp" line="46"/>
         <source>Desktop</source>
-        <translation>डेस्क्टप</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../model/placesmodel.cpp" line="53"/>
         <source>Documents</source>
-        <translation>कागजात</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../model/placesmodel.cpp" line="60"/>
         <source>Downloads</source>
-        <translation>डाउनलोड</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../model/placesmodel.cpp" line="67"/>
         <source>Music</source>
-        <translation>संगीत</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../model/placesmodel.cpp" line="74"/>
         <source>Pictures</source>
-        <translation>तस्विरहरु</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../model/placesmodel.cpp" line="81"/>
         <source>Videos</source>
-        <translation>भिडियो हरु</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../model/placesmodel.cpp" line="86"/>
         <source>Trash</source>
-        <translation>रद्दी टोकरि</translation>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
@@ -290,57 +290,57 @@
     <message>
         <location filename="../qml/Dialogs/PropertiesDialog.qml" line="28"/>
         <source>Properties</source>
-        <translation>स्वामित्व</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../qml/Dialogs/PropertiesDialog.qml" line="103"/>
         <source>Type:</source>
-        <translation>प्रकार:</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../qml/Dialogs/PropertiesDialog.qml" line="116"/>
         <source>Location:</source>
-        <translation>स्थान:</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../qml/Dialogs/PropertiesDialog.qml" line="127"/>
         <source>Size:</source>
-        <translation>आकार:</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../qml/Dialogs/PropertiesDialog.qml" line="135"/>
         <source>Calculating...</source>
-        <translation>हिसाब गर्दै......</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../qml/Dialogs/PropertiesDialog.qml" line="140"/>
         <source>Created:</source>
-        <translation>निर्मित:</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../qml/Dialogs/PropertiesDialog.qml" line="153"/>
         <source>Modified:</source>
-        <translation>बदलिएको:</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../qml/Dialogs/PropertiesDialog.qml" line="166"/>
         <source>Accessed:</source>
-        <translation>पँहुच भएको:</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../qml/Dialogs/PropertiesDialog.qml" line="188"/>
         <source>Cancel</source>
-        <translation>रद्द</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../qml/Dialogs/PropertiesDialog.qml" line="197"/>
         <source>OK</source>
-        <translation>हुन्छ</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../dialogs/propertiesdialog.cpp" line="209"/>
         <source>%1 files</source>
-        <translation>%1 फाइल हरु</translation>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
@@ -348,7 +348,7 @@
     <message>
         <location filename="../qml/main.qml" line="35"/>
         <source>File Manager</source>
-        <translation>फाइल ब्यवस्थापक</translation>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 </TS>
