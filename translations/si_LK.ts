111cb0ea2a161771729a987aff390a38d5e411ae
Hosted Weblate <hosted@weblate.org>
2021-07-25 19:34:43 +0200

    Translated using Weblate (French)
    Currently translated at 100.0% (64 of 64 strings)
    
    Translated using Weblate (Sinhala)
    
    Currently translated at 62.5% (40 of 64 strings)
    
    Added translation using Weblate (Sinhala)
    
    Co-authored-by: HelaBasa <R45XvezA@protonmail.ch>
    Co-authored-by: Hosted Weblate <hosted@weblate.org>
    Co-authored-by: Weblate <noreply@weblate.org>
    Co-authored-by: roxfr <roxfr@outlook.fr>
    Translate-URL: https://hosted.weblate.org/projects/cutefishos/filemanager/fr/
    Translate-URL: https://hosted.weblate.org/projects/cutefishos/filemanager/si/
    Translation: CutefishOS/FileManager

--- translations/si_LK.ts
+++ translations/si_LK.ts
@@ -1,27 +1,27 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="fr">
+<TS version="2.1" language="si">
 <context>
     <name>CreateFolderDialog</name>
     <message>
         <location filename="../qml/Dialogs/CreateFolderDialog.qml" line="28"/>
         <source>New folder name</source>
-        <translation>Nouveau nom de dossier</translation>
+        <translation>නව බහාලුමේ නම</translation>
     </message>
     <message>
         <location filename="../qml/Dialogs/CreateFolderDialog.qml" line="55"/>
         <source>New folder</source>
-        <translation>Nouveau dossier</translation>
+        <translation>නව බහාලුම</translation>
     </message>
     <message>
         <location filename="../qml/Dialogs/CreateFolderDialog.qml" line="70"/>
         <source>Cancel</source>
-        <translation>Annuler</translation>
+        <translation>අවලංගු කරන්න</translation>
     </message>
     <message>
         <location filename="../qml/Dialogs/CreateFolderDialog.qml" line="76"/>
         <source>OK</source>
-        <translation>OK</translation>
+        <translation>හරි</translation>
     </message>
 </context>
 <context>
@@ -29,37 +29,37 @@
     <message>
         <location filename="../helper/datehelper.cpp" line="15"/>
         <source>Now</source>
-        <translation>Maintenant</translation>
+        <translation>දැන්</translation>
     </message>
     <message>
         <location filename="../helper/datehelper.cpp" line="17"/>
         <source>1 minute ago</source>
-        <translation>il y a 1 minute</translation>
+        <translation>විනාඩි 1 කට පෙරාතුව</translation>
     </message>
     <message>
         <location filename="../helper/datehelper.cpp" line="19"/>
         <source>%1 minutes ago</source>
-        <translation>il y a %1 minutes</translation>
+        <translation>විනාඩි %1 කට පෙරාතුව</translation>
     </message>
     <message>
         <location filename="../helper/datehelper.cpp" line="23"/>
         <source>1 hour ago</source>
-        <translation>il y a 1 heure</translation>
+        <translation>පැය 1 කට පෙරාතුව</translation>
     </message>
     <message>
         <location filename="../helper/datehelper.cpp" line="25"/>
         <source>%1 hours ago</source>
-        <translation>il y a %1 heures</translation>
+        <translation>පැය %1 කට පෙරාතුව</translation>
     </message>
     <message>
         <location filename="../helper/datehelper.cpp" line="29"/>
         <source>1 day ago</source>
-        <translation>il y a 1 jour</translation>
+        <translation>දින 1 කට පෙරාතුව</translation>
     </message>
     <message>
         <location filename="../helper/datehelper.cpp" line="31"/>
         <source>%1 days ago</source>
-        <translation>il y a %1 jours</translation>
+        <translation>දින %1 කට පෙරාතුව</translation>
     </message>
 </context>
 <context>
@@ -67,7 +67,7 @@
     <message>
         <location filename="../desktop/desktopview.cpp" line="44"/>
         <source>Desktop</source>
-        <translation>Bureau</translation>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
@@ -75,22 +75,22 @@
     <message>
         <location filename="../qml/Dialogs/EmptyTrashDialog.qml" line="30"/>
         <source>File Manager</source>
-        <translation>Gestionnaire de fichiers</translation>
+        <translation>ගොනු කළමනාකරු</translation>
     </message>
     <message>
         <location filename="../qml/Dialogs/EmptyTrashDialog.qml" line="60"/>
         <source>Do you want to permanently delete all files from the Trash?</source>
-        <translation>Voulez-vous supprimer définitivement tous les fichiers de la corbeille&#xa0;?</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../qml/Dialogs/EmptyTrashDialog.qml" line="69"/>
         <source>Cancel</source>
-        <translation>Annuler</translation>
+        <translation>අවලංගු කරන්න</translation>
     </message>
     <message>
         <location filename="../qml/Dialogs/EmptyTrashDialog.qml" line="75"/>
         <source>Empty Trash</source>
-        <translation>Vider la corbeille</translation>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
@@ -98,82 +98,82 @@
     <message>
         <location filename="../model/foldermodel.cpp" line="166"/>
         <source>%1 item</source>
-        <translation>%1 élément</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../model/foldermodel.cpp" line="166"/>
         <source>%1 items</source>
-        <translation>%1 éléments</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../model/foldermodel.cpp" line="830"/>
         <source>Select All</source>
-        <translation>Tout sélectionner</translation>
+        <translation>සියල්ල තෝරන්න</translation>
     </message>
     <message>
         <location filename="../model/foldermodel.cpp" line="1042"/>
         <source>Open</source>
-        <translation>Ouvrir</translation>
+        <translation>විවෘත කරන්න</translation>
     </message>
     <message>
         <location filename="../model/foldermodel.cpp" line="1045"/>
         <source>Cut</source>
-        <translation>Couper</translation>
+        <translation>කපන්න</translation>
     </message>
     <message>
         <location filename="../model/foldermodel.cpp" line="1048"/>
         <source>Copy</source>
-        <translation>Copier</translation>
+        <translation>පිටපත් කරන්න</translation>
     </message>
     <message>
         <location filename="../model/foldermodel.cpp" line="1051"/>
         <source>Paste</source>
-        <translation>Coller</translation>
+        <translation>අලවන්න</translation>
     </message>
     <message>
         <location filename="../model/foldermodel.cpp" line="1054"/>
         <source>New Folder</source>
-        <translation>Nouveau dossier</translation>
+        <translation>නව බහාලුම</translation>
     </message>
     <message>
         <location filename="../model/foldermodel.cpp" line="1057"/>
         <source>Move To Trash</source>
-        <translation>Déplacer dans la Corbeille</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../model/foldermodel.cpp" line="1060"/>
         <source>Empty Trash</source>
-        <translation>Vider la corbeille</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../model/foldermodel.cpp" line="1063"/>
         <source>Delete</source>
-        <translation>Supprimer</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../model/foldermodel.cpp" line="1066"/>
         <source>Rename</source>
-        <translation>Renommer</translation>
+        <translation>නැවත නම් කරන්න</translation>
     </message>
     <message>
         <location filename="../model/foldermodel.cpp" line="1069"/>
         <source>Open in Terminal</source>
-        <translation>Ouvrir dans le terminal</translation>
+        <translation>අග්‍රයෙහි විවෘත කරන්න</translation>
     </message>
     <message>
         <location filename="../model/foldermodel.cpp" line="1072"/>
         <source>Set as Wallpaper</source>
-        <translation>Définir comme fond d&apos;écran</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../model/foldermodel.cpp" line="1075"/>
         <source>Properties</source>
-        <translation>Propriétés</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../model/foldermodel.cpp" line="1078"/>
         <source>Change background</source>
-        <translation>Modifier l&apos;arrière-plan</translation>
+        <translation>පසුබිම වෙනස් කරන්න</translation>
     </message>
 </context>
 <context>
@@ -181,37 +181,37 @@
     <message>
         <location filename="../qml/FolderPage.qml" line="54"/>
         <source>Empty folder</source>
-        <translation>Dossier vide</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../qml/FolderPage.qml" line="85"/>
         <source>Open</source>
-        <translation>Ouvrir</translation>
+        <translation>විවෘත කරන්න</translation>
     </message>
     <message>
         <location filename="../qml/FolderPage.qml" line="90"/>
         <source>Properties</source>
-        <translation>Propriétés</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../qml/FolderPage.qml" line="146"/>
         <source>%1 item</source>
-        <translation>%1 élément</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../qml/FolderPage.qml" line="147"/>
         <source>%1 items</source>
-        <translation>%1 éléments</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../qml/FolderPage.qml" line="153"/>
         <source>%1 selected</source>
-        <translation>%1 sélectionnés</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../qml/FolderPage.qml" line="164"/>
         <source>Empty Trash</source>
-        <translation>Vider la corbeille</translation>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
@@ -219,27 +219,27 @@
     <message>
         <location filename="../qml/OptionsMenu.qml" line="45"/>
         <source>Icons</source>
-        <translation>Icônes</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../qml/OptionsMenu.qml" line="79"/>
         <source>List</source>
-        <translation>Liste</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../qml/OptionsMenu.qml" line="115"/>
         <source>Name</source>
-        <translation>Nom</translation>
+        <translation>නම</translation>
     </message>
     <message>
         <location filename="../qml/OptionsMenu.qml" line="149"/>
         <source>Date</source>
-        <translation>Date</translation>
+        <translation>දිනය</translation>
     </message>
     <message>
         <location filename="../qml/OptionsMenu.qml" line="183"/>
         <source>Size</source>
-        <translation>Taille</translation>
+        <translation>ප්‍රමාණය</translation>
     </message>
 </context>
 <context>
@@ -247,42 +247,42 @@
     <message>
         <location filename="../model/placesmodel.cpp" line="39"/>
         <source>Home</source>
-        <translation>Accueil</translation>
+        <translation>මුල</translation>
     </message>
     <message>
         <location filename="../model/placesmodel.cpp" line="46"/>
         <source>Desktop</source>
-        <translation>Bureau</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../model/placesmodel.cpp" line="53"/>
         <source>Documents</source>
-        <translation>Documents</translation>
+        <translation>ලේඛන</translation>
     </message>
     <message>
         <location filename="../model/placesmodel.cpp" line="60"/>
         <source>Downloads</source>
-        <translation>Téléchargements</translation>
+        <translation>බාගැනීම්</translation>
     </message>
     <message>
         <location filename="../model/placesmodel.cpp" line="67"/>
         <source>Music</source>
-        <translation>Musique</translation>
+        <translation>සංගීත</translation>
     </message>
     <message>
         <location filename="../model/placesmodel.cpp" line="74"/>
         <source>Pictures</source>
-        <translation>Images</translation>
+        <translation>පින්තූර</translation>
     </message>
     <message>
         <location filename="../model/placesmodel.cpp" line="81"/>
         <source>Videos</source>
-        <translation>Vidéos</translation>
+        <translation>දෘශ්‍යක</translation>
     </message>
     <message>
         <location filename="../model/placesmodel.cpp" line="86"/>
         <source>Trash</source>
-        <translation>Corbeille</translation>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
@@ -290,57 +290,57 @@
     <message>
         <location filename="../qml/Dialogs/PropertiesDialog.qml" line="28"/>
         <source>Properties</source>
-        <translation>Propriétés</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../qml/Dialogs/PropertiesDialog.qml" line="103"/>
         <source>Type:</source>
-        <translation>Type&#xa0;:</translation>
+        <translation>වර්ගය:</translation>
     </message>
     <message>
         <location filename="../qml/Dialogs/PropertiesDialog.qml" line="116"/>
         <source>Location:</source>
-        <translation>Emplacement&#xa0;:</translation>
+        <translation>ස්ථානය:</translation>
     </message>
     <message>
         <location filename="../qml/Dialogs/PropertiesDialog.qml" line="127"/>
         <source>Size:</source>
-        <translation>Taille&#xa0;:</translation>
+        <translation>ප්‍රමාණය:</translation>
     </message>
     <message>
         <location filename="../qml/Dialogs/PropertiesDialog.qml" line="135"/>
         <source>Calculating...</source>
-        <translation>Calcul…</translation>
+        <translation>ගණනය වෙමින්...</translation>
     </message>
     <message>
         <location filename="../qml/Dialogs/PropertiesDialog.qml" line="140"/>
         <source>Created:</source>
-        <translation>Créé le&#xa0;:</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../qml/Dialogs/PropertiesDialog.qml" line="153"/>
         <source>Modified:</source>
-        <translation>Modifié le&#xa0;:</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../qml/Dialogs/PropertiesDialog.qml" line="166"/>
         <source>Accessed:</source>
-        <translation>Accès le&#xa0;:</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../qml/Dialogs/PropertiesDialog.qml" line="188"/>
         <source>Cancel</source>
-        <translation>Annuler</translation>
+        <translation>අවලංගු කරන්න</translation>
     </message>
     <message>
         <location filename="../qml/Dialogs/PropertiesDialog.qml" line="197"/>
         <source>OK</source>
-        <translation>OK</translation>
+        <translation>හරි</translation>
     </message>
     <message>
         <location filename="../dialogs/propertiesdialog.cpp" line="209"/>
         <source>%1 files</source>
-        <translation>%1 fichiers</translation>
+        <translation>ගොනු %1</translation>
     </message>
 </context>
 <context>
@@ -348,7 +348,7 @@
     <message>
         <location filename="../qml/main.qml" line="35"/>
         <source>File Manager</source>
-        <translation>Gestionnaire de fichiers</translation>
+        <translation>ගොනු කළමනාකරු</translation>
     </message>
 </context>
 </TS>
